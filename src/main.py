bf4efd1cc794dfd3877b0a25e17ef5c410d75c5a
Leandro Kellermann de Oliveira <lk.kellermann@hotmail.com>
2022-11-13 15:27:26 -0300

    fix: implementation of AvroProducer and JsonProducer.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO 
 from sensors.dht11 import DHT11
-from producer.dht11 import producer
+from producer.avro_producer import AvroProducer
+from producer.json_producer import JsonProducer
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -9,9 +10,12 @@ def main():
     Configure sensor object to execute measurements and send data to producer via REST API proxy.
     """
     dht11 = DHT11('setup/dht11.cfg')
+    dht_avro = AvroProducer(dht11)
+    dht_json = JsonProducer(dht11)
     try:
         while True:
-            producer(dht11)
+            dht_avro.produce()
+            dht_json.produce()
     except KeyboardInterrupt:
         print('End')
 
